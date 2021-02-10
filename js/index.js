2071a31c0d8743e4a00073c290653f69c3c0167a
Eva Garcia <emgg2222@gmail.com>
2021-02-10 16:13:50 +0100

    create loader

--- js/index.js
+++ js/index.js
@@ -1,4 +1,9 @@
 'use strict';
 window.addEventListener('DOMContentLoaded', (event) =>{
-
+    
+    setTimeout(() => {
+        const loader = document.querySelector('.lds-ripple');
+        loader.classList.add('hidden');
+    },2000)
+    
 })
