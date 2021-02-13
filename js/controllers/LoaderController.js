13432d65db667e261cc1c0dbe2d0509d469fd134
Eva Garcia <emgg2222@gmail.com>
2021-02-13 13:43:13 +0100

    create Base and Loader Controller

---
+++ js/controllers/LoaderController.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import BaseController from "./BaseController.js";
+
+export default class LoaderController extends BaseController {
+    showLoader () {
+        this.element.classList.remove('hidden');
+    }
+
+    hideLoader () {
+        this.element.classList.add('hidden');
+    }
+}
